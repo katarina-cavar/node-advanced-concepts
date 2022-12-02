aba5a8b3e46569ab5f1382558c9fe297733d8f2c
Katarina Cavar <katarina.cavar@cloudsensesolutions.com>
2022-12-02 15:39:30 +0100

    Lesson 30: Benchmark Refactor

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
+process.env.UV_THREADPOOL_SIZE = 1;
+const crypto = require('crypto');
 const cluster = require('cluster');
 const express = require('express');
 const app = express();
-
-function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
-}
 
 if (cluster.isMaster) {
     console.log('Master process');
@@ -13,11 +10,12 @@ if (cluster.isMaster) {
 } else {
     console.log('Child process')
     app.get('/', (req, res) => {
-        doWork(5000);
-        res.send("Hi there! Katarina");
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+            res.send("Hi there! You've reached root crypto message");
+        });
     });
     app.get('/fast', (req, res) => {
-        res.send("Hi there! Katarina");
+        res.send("Hi there! You've reached 'fast'");
     });
 
     app.listen(3000);
